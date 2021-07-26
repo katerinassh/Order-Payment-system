65bf3abe184e6f7d7190b18c1ac548ba9a15dcfc
Katerina <68004503+katerinassh@users.noreply.github.com>
2021-07-26 12:34:53 +0300

    [FEATURE] configure eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(8000, () => {
-  console.log('App listening on port 8000!')
+  console.log('App listening on port 8000!');
 });
