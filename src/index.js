cfbbad57946ece7bb2459a7b757bbe15507528b7
Katerina <68004503+katerinassh@users.noreply.github.com>
2021-07-28 17:57:26 +0300

    [FEATURE] basic graphQL setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express();
+const express = require('express');
+const graphql = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+const schema = graphql.buildSchema(`
+  type Query {
+    getHello: String
+  }
+`);
+
+const root = { getHello: () => "Hello world!" };
+
+const app = express();
+app.use('/', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(8000, () => {
   console.log('App listening on port 8000!')
