be47b29fb625bc7479a390efb6fda209a776ec69
Katerina <68004503+katerinassh@users.noreply.github.com>
2021-08-09 22:33:38 +0300

    [FEATURE] CRUD for orders with REST

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const { ordersRouter } = require('./modules/orders/orders.router');
 
 const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use(bodyParser.json());
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+app.use('/orders', ordersRouter);
 
 app.listen(8000, () => {
   console.log('App listening on port 8000!');
